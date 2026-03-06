efbb9a0516d9e0b4d7048bfa4faaf86384fa1d4e
Talles Werner Paiva <talleswernerp@gmail.com>
2026-03-06 14:21:16 -0300

    Update .gitignore to include .env and modify Vite config to load environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://webhook.tizarlabs.app/webhook',
+        target: env.API_BASE_URL || 'https://localhost:5678/webhook-test/',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
+  }
 })
